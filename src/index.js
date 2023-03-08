5c8da97242896ac6360181e1df3734dce4b21e17
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-03-07 17:01:55 -0700

    Button test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Button} from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function greeting() {
+function Greeting() {
     return <h1>Hello world</h1>
 }
 
-root.render(greeting());
+root.render(
+    <>
+    <Greeting />
+    <Button text="Hola"/>
+    <Button text="Hola 2"/>
+    <Button text="andre"/>
+    </>
+);
